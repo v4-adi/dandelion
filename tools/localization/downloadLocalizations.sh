e5219c4f78ac495b06731f762f21c065d748a464
Gregor Santner <gdev@live.de>
2016-08-07 22:52:00 +0200

    Update Translations - Brazilian

--- tools/localization/downloadLocalizations.sh
+++ tools/localization/downloadLocalizations.sh
@@ -19,8 +19,15 @@ argc=$#
 cd "$SCRIPTDIR"
 
 if [ ! -f "crowdin.yaml" ] ; then
-	echo "base_path: $(realpath '../../')" > 'crowdin.yaml'
+	echo "project_identifier: diaspora-for-android" > 'crowdin.yaml'
+	echo "base_path: $(realpath '../../')" >>'crowdin.yaml'
+	echo "api_key: DONT_PUSH_API_KEY" >>'crowdin.yaml'
 	cat "../../crowdin.yaml" >> "crowdin.yaml"
+	echo "# Add all non locality languages here" >> "crowdin.yaml"
+	echo "# (e.g. enUS, enUK, deCH, deAT will automatically go into the right folder)" >> "crowdin.yaml"
+	echo "# Otherwise e.g.  en would get added into the folder enEN (which is wrong)." >> "crowdin.yaml"
+	echo "# https://crowdin.com/page/api/language-codes contains supported language codes" >> "crowdin.yaml"
+	echo "# The first listed ones here are diffently managed by crowdin than on android" >> "crowdin.yaml"
 fi
 
 if grep -q "DONT_PUSH" "crowdin.yaml" ; then
